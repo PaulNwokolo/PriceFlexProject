6a74118af714f38683979bc21732b8e8c111cd08
Chukwuemeka Paul Nwokolo <nwokolopaul979@gmail.com>
2024-07-30 08:30:37 +0100

    Started the full projects and created the needed files

---
+++ backend/create_data.py
@@ -0,0 +1,5 @@
+# Will be using a dummy data for this project
+# This is a dymanmic pricing model named PriceFlex that aids the users to change or ajust their 
+# prices of proucts to be able to meant or have a better price 
+
+import  pandas as pd
